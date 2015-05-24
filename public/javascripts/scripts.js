c5c0f46925e7b20ef96fd26f799b973758a26577
Marion Huebner <mphuebner@gmail.com>
2015-05-23 20:34:54 -0500

    attempts email validation in user form

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,18 +1,25 @@
 function init(){
-  jQuery('#signup-form').on('submit', function(e){
-    e.preventDefault();
-    jQuery(this).find('input').text('');
-    jQuery.ajax({
-      url: '/fans',
-      method: 'post',
-      dataType: 'json',
-      data: jQuery(this).serialize(),
-      success: function(data){
-        jQuery('#fan-list').prepend( jQuery('<li>').text( data.name ) );
-      }
-    });
-  });
-}
+
+  var rege = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  if(rege.test($('#email').val() === '')){
+    console.log('form is ready');
+  } else(rege.test($('#email').val())){
+    jQuery('#signup-form').on('submit', function(e){
+      e.preventDefault();
+      jQuery(this).find('input').text('');
+
+      jQuery.ajax({
+        url: '/fans',
+        method: 'post',
+        dataType: 'json',
+        data: jQuery(this).serialize(),
+        success: function(data){
+          jQuery('#fan-list').prepend( jQuery('<li>').text( data.name ) );
+        } // end success function
+      }); // end ajax call
+    }); // end click event
+  } // end if/else statement
+} // end init function
 
 jQuery(document).ready(init);
-
